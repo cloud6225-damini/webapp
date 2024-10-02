e6d2629e14366d133322867f5f3e49ac68ad28e6
Damini Thorat <thorat.d@northeastern.edu>
2024-10-01 20:49:56 -0400

    Assignment 2 bootstrap database.

--- app/server.js
+++ app/server.js
@@ -1,30 +1,41 @@
 require('dotenv').config();
 const express = require('express');
 const healthRoutes = require('./routes/healthRoutes');
-const checkPayload = require('./middleware/routesMiddleware.js');
- 
+const validatePayload = require('./middleware/routesMiddleware.js');
+const userRoutes = require('./routes/userRoutes');
+const { createDatabase } = require('./databaseConfig/databaseConnect.js');
+
+
+createDatabase()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("Failed to initialize database,", err);
+  });
+  
+
 const app = express();
- 
+
 app.use(express.json());
- 
-app.use(checkPayload);
- 
+
+app.use(validatePayload);
+
 app.use('/healthz', healthRoutes);
- 
+
+app.use('/v1', userRoutes);
+
 app.all('/healthz', (req, res) => {
     res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.setHeader('Pragma', 'no-cache');
     res.status(405).send();
 });
- 
+
 app.use((req, res) => {
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.status(404).send();
-});
- 
-const PORT = process.env.PORT || 3000;
- 
-app.listen(PORT, () => {
-    console.log(`Server is running on the port: ${PORT}`);
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.status(404).send();
 });
